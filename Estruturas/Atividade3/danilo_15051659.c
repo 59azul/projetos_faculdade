d9dbdc6f417fed2a3adfe55f9a4de0ed96b99e8c
59azul <danilopaixao00@gmail.com>
2016-03-21 14:29:45 -0300

    adição estrutura na atividade 3

--- Estruturas/Atividade3/danilo_15051659.c
+++ Estruturas/Atividade3/danilo_15051659.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+typedef taluno aluno;
+struct taluno{
+    int ra;
+    char nome[50];
+    taluno *prox;
+};
 
 
 int main(void)
 {
-    
-    
-    
+
+
+
     return 0;
 }
