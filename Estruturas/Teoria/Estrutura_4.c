7c1f8cf376d53e3309f5439fd34cdd056add7240
59azul <danilopaixao00@gmail.com>
2016-03-21 13:46:06 -0300

    Adição aulas teoricas

---
+++ Estruturas/Teoria/Estrutura_4.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef tno no;
+
+struct tno{
+    int info;
+    //demais campos
+    struct tno *prox, *ant; // Para listas duplamente ligadas
+};
+
+int main(void)
+{
+    no no1;
+
+    
+    return 0;
+}
