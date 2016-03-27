7698b15123bca4369a63245e2162cebe60140831
59azul <danilopaixao00@gmail.com>
2016-03-27 19:35:30 +0000

    Adição makefile em ativade 3

--- Estruturas/Atividade3/danilo_15051659.c
+++ Estruturas/Atividade3/danilo_15051659.c
@@ -8,14 +8,14 @@ struct taluno{
     struct taluno *prox;
 };
 
+// para remover guardar o anterior
+
 void cadastroAluno(aluno *inicio)
 {
-    char nome[50];
     int ra;
     
     printf("Digite o ra do aluno a ser cadastrado: ");
     scanf("%d", &ra);
-    
     
     inicio->ra = ra;
     
@@ -23,9 +23,24 @@ void cadastroAluno(aluno *inicio)
     __fpurge(stdin);
     fgets(inicio->nome, 50, stdin);
     
-    
-    
+    inicio->prox=NULL;
+}
 
+void procuraCadastroAluno(aluno *inicio) // Função procura o último elemento da lista alunos
+{
+    aluno *aux;
+    aux=inicio->prox;
+    if(aux)
+    {
+        while(aux)
+        {
+            aux = aux->prox;
+        }
+        
+    }
+    aux->prox = (aluno *)malloc(sizeof (aluno));
+    
+    cadastroAluno(aux->prox);
 }
 
 int main(void)
@@ -35,6 +50,9 @@ int main(void)
     aux = (aluno *)malloc(sizeof (aluno));
     
     cadastroAluno(aux);
+    
+    
+    
     
     return 0;
 }
