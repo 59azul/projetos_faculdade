4e77d6d1a99e0f9fe436437678412ba7148a1fca
59azul <danilopaixao00@gmail.com>
2016-06-10 14:15:21 -0300

    Inicio projeto prolog

---
+++ Paradigmas/Projeto_Prolog/baseDados.pl
@@ -0,0 +1,6 @@
+% vôo(origem,destino,código,partida,(dia_chegada,horario_chegada),
+% número_de_escalas, companhia,[dias]).
+vôo(sao_paulo, mexico, a6, 17:00, (mesmo, 23:00), 0, gol, [segunda, terca, sabado]).
+vôo(sao_paulo, nova_york, a84, 16:00 , (mesmo, 22:00), 0, tam, [terca, quarta, sexta].
+vôo(sao_paulo, lisboa, a32, 14:00, (mesmo, 20:30), 2, azul, [quarta, sexta, sababo]).
+vôo(sao_paulo, madrid, a68, ).
