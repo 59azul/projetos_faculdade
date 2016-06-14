c7c4a7034ec7e71d62cf4610ace22d13f25754fd
59azul <danilopaixao00@gmail.com>
2016-06-14 11:21:44 -0300

    Fatos Prolog

--- Paradigmas/Projeto_Prolog/baseDados.pl
+++ Paradigmas/Projeto_Prolog/baseDados.pl
@@ -1,9 +1,12 @@
 % vôo(origem,destino,código,partida,(dia_chegada,horario_chegada),
 % número_de_escalas, companhia,[dias]).
 vôo(sao_paulo, mexico, a6, 17:00, (mesmo, 23:00), 0, gol, [segunda, terca, sabado]).
-vôo(sao_paulo, nova_york, a84, 16:00 , (mesmo, 22:00), 0, tam, [terca, quarta, sexta].
+vôo(sao_paulo, nova_york, a84, 16:00 , (mesmo, 22:00), 1, tam, [terca, quarta, sexta].
 vôo(sao_paulo, lisboa, a32, 14:00, (mesmo, 20:30), 2, azul, [quarta, sexta, sababo]).
-vôo(sao_paulo, madrid, a68, 20:30, (proximo, 03:00), 1, tam, [sexta, sabado, domingo]).
-vôo(sao_paulo, londres, a54, 21:00, (proximo, 05:00), 2, gol, [segunda, sexta, domingo]).
-vôo(sao_paulo, paris, a48, 00:00, (mesmo, 06:30), 1, azul, [terca, quinta, sexta]).
-vôo(mexico, nova_york, b66, 15:00, (mesmo, 17:00), 1, tam, [quarta, quinta, sexta, sabado]).
+vôo(sao_paulo, madrid, a68, 20:30, (proximo, 03:00), 3, tam, [sexta, sabado, domingo]).
+vôo(sao_paulo, londres, a54, 21:00, (proximo, 05:00), 4, gol, [segunda, sexta, domingo]).
+vôo(sao_paulo, paris, a48, 00:00, (mesmo, 06:30), 5, azul, [terca, quinta, sexta]).
+vôo(mexico, nova_york, b66, 15:00, (mesmo, 17:00), 0, tam, [quarta, quinta, sexta, sabado]).
+vôo(mexico, madrid, b65, 20:00, (proximo, 01:30), 1, azul, [segunda, quarta, quinta]).
+vôo(nova_york, londres, c64, 13:00, (mesmo, 17:00), 0, gol, [terca, quinta, sabado]).
+vôo()
