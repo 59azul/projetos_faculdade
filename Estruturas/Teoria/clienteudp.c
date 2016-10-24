d1ef5d77a5b166ebc65746d3783d00109e4dfb7e
59azul <59azul@users.noreply.github.com>
2016-10-24 09:29:54 -0200

    Cliente e servidor Timestamp

---
+++ Estruturas/Teoria/clienteudp.c
@@ -0,0 +1,48 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <strings.h>
+
+#define DATA "Esta eh a mensagem que quero enviar"
+
+main(argc, argv)
+     int argc;
+     char *argv[];
+{
+	int sock, length, cont;
+	struct sockaddr_in name;
+	char buf[1024];
+	struct hostent *hp, *gethostbyname();
+
+        /* Cria o socket de comunicacao */
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if(sock<0) {
+	/*
+	/- houve erro na abertura do socket
+	*/
+		perror("opening datagram socket");
+		exit(1);
+	}
+	/* Associa */
+        hp = gethostbyname(argv[1]);
+        if (hp==0) {
+            fprintf(stderr, "%s: unknown host ", argv[1]);
+            exit(2);
+        }
+        bcopy ((char *)hp->h_addr, (char *)&name.sin_addr, hp->h_length);
+	name.sin_family = AF_INET;
+	name.sin_port = htons(atoi(argv[2]));
+
+	/* Envia */
+	if (sendto (sock,DATA,sizeof DATA, 0, (struct sockaddr *)&name,
+                    sizeof name)<0)
+                perror("sending datagram message");
+	if (recvfrom(sock,(char *)&cont,sizeof cont, 0, (struct sockaddr *)&name, &length)<0)
+		            perror("receiving datagram packet");
+			printf("  %d\n", cont);
+        close(sock);
+        exit(0);
+}
