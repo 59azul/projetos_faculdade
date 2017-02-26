e36c6723c1b447c8d0a2bca3870dfa3cbba876aa
59azul <danilopaixao00@gmail.com>
2017-02-26 17:24:36 -0300

    Criação do arquivo fonte C do projeto de Paradigmas

---
+++ Paradigmas/Projeto_C/proj_17.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*  Projeto de cadastro de alunos, disciplinas e professores.
+*   Tarefas:
+*   - Cadastro de alunos
+*   - Cadastro de disciplinas
+*   - Cadastro de professores
+*   - Matricular um conjunto de alunos em um conjunto de disciplinas
+*   - Cancelar a matricula de um conjunto de alunos em um conjunto de disciplinas
+*   - Vincular um conjunto de professores à um conjunto de disciplinas
+*   - Imprimir lista de todos os alunos
+*   - Imprimir lista de todas as disciplinas
+*   - Imprimir lista de todos os professores
+*   - Imprimir lista de alunos matriculados em uma disciplina
+*   - Imprimir lista de disciplinas que um aluno está matriculado
+*   - Imprimir lista de professores vinculados à uma disciplina
+*   - Imprimir lista de disciplinas ministradas por um professor
+*/
+
+
+
+int main()
+{
+    
+
+
+    printf("O que deseja fazer? \n");
+    
+
+
+    return 0;
+}
