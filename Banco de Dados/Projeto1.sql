80dbd6e17c8f78ff0df233f0f964c3aff1dee887
59azul <59azul@users.noreply.github.com>
2017-05-09 14:33:55 -0300

    Inserção de cursos

--- Banco de Dados/Projeto1.sql
+++ Banco de Dados/Projeto1.sql
@@ -52,3 +52,46 @@ alter table matricula add constraint fk_disc_matricula foreign key (cod_disc) re
 
 alter table matricula add constraint pk_matricula primary key (ra, cod_disc);
 
+insert into cursos values ('Matemática', 'Campus 1', 1);
+
+insert into cursos values ('Geografia', 'Campus 1', 2);
+
+insert into cursos values ('Medicina', 'Campus 2', 3);
+
+insert into alunos values (1, 1, 'Mateus', to_date('21/05/1996', 'dd/mm/yyyy'), 'Campinas');
+
+insert into alunos values (1, 2, 'João', to_date('14/12/1995', 'dd/mm/yyyy'), 'São Paulo');
+
+insert into alunos values (1, 3, 'Joaquim', to_date('28/01/1992', 'dd/mm/yyyy'), 'Campinas');
+
+insert into alunos values (1, 4, 'Bruna', to_date('22/11/1994', 'dd/mm/yyyy'), 'Santos');
+
+insert into alunos values (2, 5, 'Maria', to_date('17/07/1996', 'dd/mm/yyyy'), 'Campinas');
+
+insert into alunos values (3, 6, 'Paulo', to_date('25/08/1996', 'dd/mm/yyyy'), 'São Paulo');
+
+insert into alunos values (2, 7, 'Marcelo', to_date('15/10/1995', 'dd/mm/yyyy'), 'Campinas');
+
+insert into alunos values (1, 8, 'Beatriz', to_date('23/02/1995','dd/mm/yyyy'), 'Campinas');
+
+insert into alunos values (2, 9, 'Leonardo', to_date('19/07/1996','dd/mm/yyyy'), 'Rio de Janeiro');
+
+insert into alunos values (1, 10, 'Alex', to_date('30/01/1996','dd/mm/yyyy'), 'Campinas');
+
+insert into alunos values (3, 11, 'Carlos', to_date('31/03/1995','dd/mm/yyyy'), 'São Paulo');
+
+insert into alunos values (3, 12, 'José', to_date('15/06/1996','dd/mm/yyyy'), 'Campinas');
+
+insert into alunos values (2, 13, 'Carolina', to_date('26/04/1996','dd/mm/yyyy'), 'Campinas');
+
+insert into alunos values (1, 14, 'Daniel', to_date('22/09/1995','dd/mm/yyyy'), 'Campinas');
+
+insert into alunos values (1, 15, 'Luciana', to_date('31/12/1994','dd/mm/yyyy'), 'São Paulo');
+
+insert into alunos values (2, 16, 'Lúcio', to_date('27/02/1996','dd/mm/yyyy'), 'Campinas');
+
+insert into alunos values (1, 17, 'Letícia', to_date('10/05/1996','dd/mm/yyyy'), 'Campinas');
+
+insert into alunos values (3, 18, '', to_date('06/06/1996','dd/mm/yyyy'), 'Santos');
+
+insert into alunos values (, , '', to_date('','dd/mm/yyyy'), '');
