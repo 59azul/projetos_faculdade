78a385431027e409ffc7774a278a1a070865eb50
59azul <danilopaixao00@gmail.com>
2017-06-09 12:23:10 -0300

    Correção Base de Dados - Prolog

--- Paradigmas/Projeto_Prolog/baseDados.pl
+++ Paradigmas/Projeto_Prolog/baseDados.pl
@@ -1,77 +1,122 @@
-% voo(origem,destino,codigo,partida,(dia_chegada,horario_chegada),
-% numero_de_escalas, companhia,[dias]).
-voo(sao_paulo, mexico, a6, 17:00, 23:00, 0, gol, [segunda, terca, sabado]).
-voo(sao_paulo, mexico, a7, 17:00, 22:30, 0, tam, [segunda, quarta, sexta]).
-voo(sao_paulo, mexico, a8, 19:20, 00:30, 0, azul, [terca, quinta, domingo]).
-voo(sao_paulo, nova_york, a84, 16:00 , 22:00, 0, tam, [terca, quarta, sexta]).
-voo(sao_paulo, lisboa, a32, 14:00, 20:30, 0, azul, [quarta, sexta, sababo]).
-voo(sao_paulo, madrid, a68, 20:30, 03:00, 0, tam, [sexta, sabado, domingo]).
-voo(sao_paulo, londres, a54, 21:00, 05:00, 0, gol, [segunda, sexta, domingo]).
-voo(sao_paulo, paris, a48, 00:00, 06:30, 0, azul, [terca, quinta, sexta]).
-voo(mexico, nova_york, b66, 15:00, 17:00, 0, tam, [quarta, quinta, sexta, sabado]).
-voo(mexico, madrid, b65, 20:00, 01:30, 0, azul, [segunda, quarta, quinta]).
-voo(nova_york, londres, c64, 13:00, 17:00, 0, gol, [terca, quinta, sabado]).
-voo(londres, lisboa, d56, 12:00, 14:00, 0, tam, [segunda, terca, sexta]).
-voo(londres, paris, d65, 15:00, 16:30, 0, azul, [terca, quinta, sababdo]).
-voo(londres, estocolmo, d84, 12:30, 15:00, 0, tam, [segunda, quarta, domingo]).
-voo(madrid, paris, e49, 14:00, 15:30, 0, gol, [terca, quarta, quinta, sabado]).
-voo(madrid, roma, e98, 16:00, 17:00, 0, tam, [quarta, sexta, sabado, domingo]).
-voo(madrid, frankfurt, e82, 17:00, 18:30, 0, azul, [segunda, terca, sexta, domingo]).
-voo(frankfurt, estocolmo, d65, 15:00, 16:30, 0, azul, [segunda, quarta, sexta]).
-voo(frankfurt, roma, d15, 12:00, 14:00, 0, tam, [terca, quinta, sexta, sabado, domingo]).
+% voo(origem, destino, codigo, partida, (dia_chegada, horario_chegada),
+% numero_de_escalas,  companhia, [dias]).
+%Sao_Paulo
+voo(sao_paulo, mexico, a01, 19:30, 23:30, 0, gol, [quarta]).
+voo(sao_paulo, nova_york, a02, 17:15, 01:30, 0, tam, [segunda, terca, sabado]).
+voo(sao_paulo, lisboa, a03, 21:30, 03:30, 0,  azul, [sexta, sabado]).
+voo(sao_paulo, madrid, a04, 08:15, 12:30, 0,  azul, [quarta, sexta]).
+voo(sao_paulo, londres, a05, 16:30, 23:30, 0, tam, [domingo, quinta, sexta, sabado]).
+voo(sao_paulo, paris, a06, 21:45, 04:50, 0,  azul, [segunda, sabado]).
 
-maior(X,Y,X) :- X>Y, !.
-maior(_,Y,Y).
+%Mexico
+voo(mexico, nova_york, b01, 14:00, 19:30, 0, gol, [domingo, quarta, sexta]).
+voo(mexico, paris, b02, 12:00, 23:30, 0, tam, [terca, sabado, domingo]).
+voo(mexico, sao_paulo, b03, 21:30, 04:10, 0,  azul, [quarta, quinta]).
 
-menor(X,Y,X) :- X<Y, !.
-menor(_,Y,Y).
+%Nova_York
+voo(nova_york, londres, c01, 20:15, 00:30, 0,  azul, [domingo, segunda, terca, sexta, sabado]).
+voo(nova_york, sao_paulo, c02, 21:45, 09:10, 0, tam, [segunda, quinta]).
+voo(nova_york, mexico, c03, 10:30, 23:00, 0, gol, [domingo, sabado]).
 
-acrescentarFim(X,[],[X]).
-acrescentarFim(X,[Y|L1],[Y|L2]):- acrescentarFim(X,L1,L2).
+%Londres
+voo(londres, lisboa, d01, 05:10, 8:30, 0, gol, [quinta, sexta]).
+voo(londres, paris, d02, 15:00, 17:50, 0, tam, [domingo, segunda, sexta, sabado]).
+voo(londres, estocolmo, d03, 14:30, 16:00, 0, tam, [segunda, sexta]).
+voo(londres, nova_york, d04, 19:20, 02:40, 0,  azul, [segunda, sexta, sabado]).
+voo(londres, sao_paulo, d05, 10:30, 00:10, 0, gol, [segunda]).
 
-pertence_lista(X,[X|_]).
-pertence_lista(X,[_|Cauda]) :- pertence_lista(X,Cauda).
+%Lisboa
+voo(lisboa, londres, e01, 08:30, 16:50, 0,  azul, [terca, quinta]).
+voo(lisboa, sao_paulo, e02, 15:55, 01:20, 0, tam, [quinta, sabado]).
 
-voo_direto(Origem, Destino, Companhia, Dia, Horario) :-
-	voo(Origem, Destino, _, Horario, _, 0, Companhia, Lista), pertence_lista(Dia, Lista).
+%Paris
+voo(paris, londres, f01, 10:00, 16:30, 0, tam, [quarta, quinta]).
+voo(paris, sao_paulo, f02, 00:30, 19:20, 0, tam, [domingo, segunda, sexta, sabado]).
+voo(paris, madrid, f03, 23:30, 02:45, 0,  azul, [domingo, sexta]).
 
-roteiro(Origem, Destino, ListaCodigos) :-
-	voo(Origem, Destino, Codigo, _, _, _, _, _),
-	acrescentarFim(Codigo,[],ListaCodigos).
-roteiro(Origem, Destino, ListaCodigos) :-
-	voo(Conexao, Destino, Codigo, _, _, _, _, _),
-	roteiro(Origem,Conexao, Lista),
-	acrescentarFim(Codigo,Lista,ListaCodigos).
+%Estocolmo
+voo(estocolmo, londres, g01, 07:15, 13:50, 0, tam, [segunda, terca, quinta]).
+voo(estocolmo, frankfurt, g02, 20:40, 03:30, 0, gol, [quinta, sabado]).
 
-filtra_voo_dia_semana(Origem, Destino, DiaSemana, HorarioSaida, HorarioChegada, Companhia) :-
-	voo(Origem, Destino, _, HorarioSaida,HorarioChegada, 0, Companhia, Lista),
-	pertence_lista(DiaSemana, Lista).
+%Roma
+voo(roma, madrid, h01, 18:30, 23:300, 0,  azul, [segunda, quarta]).
+voo(roma, frankfurt, h02, 23:55, 06:50, 0, gol, [quinta, sabado]).
 
-duracao(Hora_i:Min_i, Hora_f:Min_f, Dura_h:Dura_m) :-
+%Madrid
+voo(madrid, paris, i01, 15:35, 18:30, 0,  azul, [quinta, sexta]).
+voo(madrid, roma, i02, 15:00, 14:50, 0, tam, [segunda, sexta, sabado]).
+voo(madrid, frankfurt, i03, 17:30, 23:10, 0, gol, [domingo, sexta, sabado]).
+voo(madrid, sao_paulo, i04, 05:00, 20:30, 0,  azul, [quintan, sabado]).
+voo(madrid, mexico, i05, 19:50, 02:40, 0, tam, [domingo, terca, quinta]).
+
+%Frankfurt
+voo(frankfurt, estocolmo, j01, 10:35, 16:30, 0, tam, [quinta, sexta, sabado]).
+voo(frankfurt, roma, j02, 17:30, 23:10, 0,  azul, [segunda, sexta, sabado]).
+voo(frankfurt, madrid, j03, 07:30, 13:25, 0, gol, [terca, sabado]).
+
+maior(X, Y, X) :- X>Y,  !.
+maior(_, Y, Y).
+
+menor(X, Y, X) :- X<Y,  !.
+menor(_, Y, Y).
+
+acrescentarFim(X, [], [X]).
+acrescentarFim(X, [Y|L1], [Y|L2]):- acrescentarFim(X, L1, L2).
+
+pertence_lista(X, [X|_]).
+pertence_lista(X, [_|Cauda]) :- pertence_lista(X, Cauda).
+
+voo_direto(Origem,  Destino,  Companhia,  Dia,  Horario) :-
+	voo(Origem,  Destino,  _,  Horario,  _,  0,  Companhia,  Lista),
+	pertence_lista(Dia,  Lista).
+
+roteiro(Origem,  Destino,  ListaCodigos) :-
+	voo(Origem,  Destino,  Codigo,  _,  _,  _,  _,  _),
+	acrescentarFim(Codigo, [], ListaCodigos).
+roteiro(Origem,  Destino,  ListaCodigos) :-
+	voo(Conexao,  Destino,  Codigo,  _,  _,  _,  _,  _),
+	roteiro(Origem, Conexao,  Lista),
+	acrescentarFim(Codigo, Lista, ListaCodigos).
+
+filtra_voo_dia_semana(Origem,  Destino,  DiaSemana,  HorarioSaida,  HorarioChegada,  Companhia) :-
+	voo(Origem,  Destino,  _,  HorarioSaida, HorarioChegada,  0,  Companhia,  Lista),
+	pertence_lista(DiaSemana,  Lista).
+
+duracao(Hora_i:Min_i,  Hora_f:Min_f,  Dura_h:Dura_m) :-
 	Hora_f < Hora_i,
 	Min_f < Min_i,
 	Dura_h is Hora_f + 23 - Hora_i,
 	Dura_m is Min_f + 60 - Min_i,
 	!.
-duracao(Hora_i:Min_i, Hora_f:Min_f, Dura_h:Dura_m) :-
+duracao(Hora_i:Min_i,  Hora_f:Min_f,  Dura_h:Dura_m) :-
 	Hora_f < Hora_i,
 	Dura_h is Hora_f + 24 - Hora_i,
-	Dura_m is Min_f - Min_i, !.
-duracao(Hora_i:Min_i, Hora_f:Min_f, Dura_h:Dura_m) :-
+	Dura_m is Min_f - Min_i,  !.
+duracao(Hora_i:Min_i,  Hora_f:Min_f,  Dura_h:Dura_m) :-
 	Min_f < Min_i,
 	Dura_h is Hora_f - 1 - Hora_i,
-	Dura_m is Min_f + 60 - Min_i, !.
-duracao(Hora_i:Min_i, Hora_f:Min_f, Dura_h:Dura_m) :-
+	Dura_m is Min_f + 60 - Min_i,  !.
+duracao(Hora_i:Min_i,  Hora_f:Min_f,  Dura_h:Dura_m) :-
 	Dura_h is Hora_f - Hora_i,
 	Dura_m is Min_f - Min_i.
 
 
-menorDuracao(Origem, Destino, Dia, HorarioSaida, HorarioChegada, Companhia) :-
-		filtra_voo_dia_semana(Origem, Destino, Dia, HorarioSaida, HorarioChegada, Companhia),
-		duracao(HorarioSaida,HorarioChegada, Duracao).
+menorDuracao(Origem,  Destino,  Dia,  HorarioSaida,  HorarioChegada,  Companhia,  ListaDuracoes) :-
+	voo(Origem,  Destino,  Codigo,  HorarioSaida, HorarioChegada,  0,  Companhia,  Lista),
+	pertence_lista(Dia,  Lista),
+	duracao(HorarioSaida, HorarioChegada,  Duracao),
+	listaDuracao([Duracao|Codigo], [], ListaDuracoes).
 
 
 
-menordalista(M,[M|[]]).
-menordalista(M1,[Head|Tail]):- menordalista(M,Tail),
-	menor(M,Head, M1).
+
+listaDuracao(X, [], [X]).
+listaDuracao(X, [Y|L1], [Y|L2]):-
+	pertence_lista(X, [Y|L1]), !.
+listaDuracao(X, [Y|L1], [Y|L2]):-
+	listaDuracao(X, L1, L2).
+
+
+menordalista(M, [M|[]]).
+menordalista(M1, [Head|Tail]):- menordalista(M, Tail),
+	menor(M, Head,  M1).
